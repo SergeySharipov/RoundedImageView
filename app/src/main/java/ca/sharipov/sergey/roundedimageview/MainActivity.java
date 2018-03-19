c27af63b2ad742ab5bc658ea1c78fad3e53438e3
Sergey-PC <sharipov14@gmail.com>
2018-03-19 01:23:44 -0400

    Added rings for RoundedImageView

--- app/src/main/java/ca/sharipov/sergey/roundedimageview/MainActivity.java
+++ app/src/main/java/ca/sharipov/sergey/roundedimageview/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private CardView cardViewForImage;
-    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +18,45 @@ public class MainActivity extends AppCompatActivity {
 
         //ImageView roundedImageView = findViewById(R.id.rounded_image_view);
         cardViewForImage = findViewById(R.id.card_view_for_image);
-        seekBar = findViewById(R.id.seek_bar);
 
         setupSeekBar();
+
+        setupRadioGroup();
+    }
+
+    private void setupRadioGroup() {
+        RadioGroup radioGroup = findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (radioGroup.getCheckedRadioButtonId()){
+                    case R.id.radio_btn_no_rings:
+                        cardViewForImage.setForeground(null);
+                        break;
+                    case R.id.radio_btn_one_ring:
+                        cardViewForImage.setForeground(getResources().getDrawable(R.drawable.one_ring));
+                        break;
+                    case R.id.radio_btn_two_rings:
+                        cardViewForImage.setForeground(getResources().getDrawable(R.drawable.two_rings));
+                        break;
+                }
+            }
+        });
     }
 
     private void setupSeekBar() {
+        SeekBar seekBar = findViewById(R.id.seek_bar);
+
         seekBar.setMax(100);
 
         int progress = 50;
         seekBar.setProgress(progress);
-        setRoundedImageViewSize(progress*5);
+        setRoundedImageViewSize(progress * 5);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                setRoundedImageViewSize(i*5);
+                setRoundedImageViewSize(i * 5);
             }
 
             @Override
